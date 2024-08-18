a6f9fa5ebb0c79d4a043572f51222f925e1f66f7
Yossi Shwartz <yossi.shwartz@dynamicyield.com>
2024-08-18 14:03:16 +0300

    f7

--- index.js
+++ index.js
@@ -20,50 +20,52 @@ script2.src = `//cdn.dynamicyield.com/api/${sectionId}/api_static.js`;
    const widgetIdFromReq = urlParams.get('w') || '365301';
 const init = () => {
     document.addEventListener('DOMContentLoaded', () => {      
-      const muse = new ShoppingMuse({
-        color: '#ff0000',
-        currencySymbol: '$',
-        disclaimer: 'To create a box shadow on the top side of an element, adjust the y-offset value to a negative number.',
-        disclaimerText1 : 'Terms of Use',
-        disclaimerText2 : 'Privacy Policy',
-        disclaimerUrl1 : 'www.google.com',
-        disclaimerUrl2 : 'https://www.google.com',
-        elementsToHide: [],
-        feedId: feedIdFromReq,
-        selectedStrategy,
-        font : 'Inter',
-  
-        image: 'https://logowik.com/content/uploads/images/t_hm9158.logowik.com.webp',
-  
-        isQuickViewEnabled: true,
-  
-        isTestModeEnabled: true,
-  
-        name: 'Demo site',
-  
-        placeholderText: 'What are you looking for?',
-  
-        previousPriceColumn: 'sale_price',
-  
-        priceField: 'price',
-        primaryProductText : 'name',
-        quickViewLoaderDuration: 1,
-  
-        // secondaryColor: 'red',
-        sectionId: sectionIdFromReq,
-  
-        // selectedStrategy: '258333',
-        similarItemsResponse: 'Here are some simi items',
-        suggestion1: 'Summer wedding style inspiration',
-        suggestion2: 'I need a trendy handbag',
-        suggestion3: 'Build a minimalist capsule that will last',
-        suggestion4: 'Surprise me!',
-        suggestion5 : 'Surprise me again',
-        suggestion6: '',
-        welcomeMessage: 'eresres',
-        widgetId: widgetIdFromReq 
-      });
-      muse.openDyChat();
+      setTimeout(()=>{
+        const muse = new ShoppingMuse({
+          color: '#ff0000',
+          currencySymbol: '$',
+          disclaimer: 'To create a box shadow on the top side of an element, adjust the y-offset value to a negative number.',
+          disclaimerText1 : 'Terms of Use',
+          disclaimerText2 : 'Privacy Policy',
+          disclaimerUrl1 : 'www.google.com',
+          disclaimerUrl2 : 'https://www.google.com',
+          elementsToHide: [],
+          feedId: feedIdFromReq,
+          selectedStrategy,
+          font : 'Inter',
+    
+          image: 'https://logowik.com/content/uploads/images/t_hm9158.logowik.com.webp',
+    
+          isQuickViewEnabled: true,
+    
+          isTestModeEnabled: true,
+    
+          name: 'Demo site',
+    
+          placeholderText: 'What are you looking for?',
+    
+          previousPriceColumn: 'sale_price',
+    
+          priceField: 'price',
+          primaryProductText : 'name',
+          quickViewLoaderDuration: 1,
+    
+          // secondaryColor: 'red',
+          sectionId: sectionIdFromReq,
+    
+          // selectedStrategy: '258333',
+          similarItemsResponse: 'Here are some simi items',
+          suggestion1: 'Summer wedding style inspiration',
+          suggestion2: 'I need a trendy handbag',
+          suggestion3: 'Build a minimalist capsule that will last',
+          suggestion4: 'Surprise me!',
+          suggestion5 : 'Surprise me again',
+          suggestion6: '',
+          welcomeMessage: 'eresres',
+          widgetId: widgetIdFromReq 
+        });
+        muse.openDyChat();
+      },0)
     });
   };
   //   // window.DYO.smartObject('muse dynamic content', {
